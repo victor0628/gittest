ede27052d3167851e1fba3174a59a8ae4259a21d
victor <lerchvictor31@gmail.com>
2025-02-11 16:28:07 +0100

    modification en un jeu

--- randnum.py
+++ randnum.py
@@ -1,3 +1,10 @@
 import random
-alea=random.randint(1, 9)
-print("Generated number:", alea)
+
+def parier(n):
+    alea=random.randint(1, 9)
+    if n==alea:
+        return "tu as parié juste !! Le bon chiffre était : "+str(alea)
+        
+    else:
+        return "Dommage, ce n'est pas le bon chiffre...Le bon chiffre était : "+str( alea)
+    
